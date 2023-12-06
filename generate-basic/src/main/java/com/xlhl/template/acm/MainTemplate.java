d978a6361749248d23c7da3668b7444553b8b8a9
xlhl <Yz15070004472@163.com>
2023-12-06 17:07:08 +0800

    Create Basic Code

---
+++ generate-basic/src/main/java/com/xlhl/template/acm/MainTemplate.java
@@ -0,0 +1,29 @@
+package com.xlhl.template.acm;
+
+import java.util.Scanner;
+
+/**
+ * ACM 输入模板（多数之和）
+ *
+ * @author xlhl
+ * @date 2023/12/6 17:10
+ */
+public class MainTemplate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("已空格分隔 每个数字求和：");
+        while (scanner.hasNext()) {
+            String inputStr = scanner.nextLine();
+            String[] inputNum = inputStr.split(" ");
+
+            int sum = 0;
+            for (String str : inputNum) {
+                sum += Integer.parseInt(str);
+            }
+
+            System.out.println("Sum: " + sum);
+        }
+
+        scanner.close();
+    }
+}
